a6da62f618ebbee0ed57b6023f271998f66d13a7
Ganesh-kaarthik <110884217+Ganesh-kaarthik@users.noreply.github.com>
2022-08-17 22:27:33 +0530

    Add files via upload

---
+++ Resume.js
@@ -0,0 +1,47 @@
+let Resume={
+    "basics information": {
+      "name": "GANESH KAARTHIK J",
+      "phone": 6374532578,
+      "email": "ganeshkaarthikmessi@gamil.com",
+      "degree": "B.Com(A&F)",
+      "location": {
+        "address": "95/28,Angalamman koil street, Kumarsamypatty, Salem-7",
+        "pincode": 636007,
+        "city": "Salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Ganesh-kaarthik",
+          "linkedin":"https://in.linkedin.com/in/ganesh-kaarthik-79b32a1b7"
+        }
+      ]
+    },
+    "education": [
+      {
+        "college": "JAIRAM ARTS AND SCIENCE COLLEGE",
+        "department": "B.Com(A&F)",
+        "cgpa": 7.0,
+        "batch": 2018-2021
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginner",
+      }
+    ],
+    "languages": [
+      {
+        "language": "tamil, english",
+      }
+    ],
+    "hobbies": [
+      {
+        "name": "watching informative videos,Reading books",
+      }
+    ]
+  }
+  console.log(Resume);
+  
